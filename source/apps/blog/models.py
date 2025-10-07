f8226b7942f6909a07a0a5d67c408ec76a6a011b
afshinx76 <afshin.kh.dev76@gmail.com>
2025-10-07 22:02:29 +0330

    ADD:add blog models and admin

--- source/apps/blog/models.py
+++ source/apps/blog/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from user.models import User
 
-# Create your models here.
+
+
+class BlogCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(BlogCategory, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class BlogComment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.blog
+
+class BlogTag(models.Model):
+    name = models.CharField(max_length=100)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class BlogReview(models.Model):
+    name = models.CharField(max_length=100)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
